9ca3df40694677dcfacdd35fcab9ad7a942b016a
SirMarkus <marcosbarreroherrero@gmail.com>
2026-06-08 13:00:44 +0200

    chore: initial commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { betterAuth } from "better-auth";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "#/db";
+
+export const auth = betterAuth({
+	emailAndPassword: {
+		enabled: true,
+	},
+	database: drizzleAdapter(db, {
+		provider: "pg", // or "pg" or "mysql"
+	}),
+	plugins: [tanstackStartCookies()],
+});
