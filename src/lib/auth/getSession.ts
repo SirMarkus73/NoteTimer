557b44999e6101451a7ad83c0d0178ad0caa8b89
SirMarkus <marcosbarreroherrero@gmail.com>
2026-06-27 17:59:03 +0200

    fix(session): solucionar error de importacion

--- src/lib/auth/getSession.ts
+++ src/lib/auth/getSession.ts
@@ -1,6 +1,6 @@
 import { createIsomorphicFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
-import { auth } from "#/lib/auth";
+import { auth } from "#/lib/auth.ts";
 import { authClient } from "#/lib/auth-client";
 
 export const getSession = createIsomorphicFn()
